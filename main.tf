33b4bc3605b005cb1cbe7851a44aad95d6cf02bc
Gyllenhammar <Gyllenhammar@users.noreply.github.com>
2022-03-10 10:35:59 +0100

    initial commit

---
+++ main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = "~>1.7.0"
+    }
+  }
+}
+
+provider "kubectl" {
+  apply_retry_count = 3
+  config_path       = var.kubeconfig_path
+}
+
+data "kubectl_file_documents" "docs" {
+  content = file(var.manifest_file_path)
+}
+
+resource "kubectl_manifest" "sentence-resource" {
+  for_each  = data.kubectl_file_documents.docs.manifests
+  yaml_body = each.value
+}
---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "kubeconfig_path" {
+  description = "File path of kubeconfig file."
+  type        = string
+}
+
+variable "manifest_file_path" {
+  description = "File path of kubernetes manifest yaml file."
+  type        = string
+}
